09f1aec57cdcaa1d073d03f98001474c900f700c
Parun Ngamcharoen <141175522+ParunNg@users.noreply.github.com>
2024-02-08 18:16:31 +0700

    Update

--- app/app.py
+++ app/app.py
@@ -1,28 +1,43 @@
 from flask import Flask, render_template, request
-from translator import Seq2SeqTransformer
-from pythainlp.tokenize import Tokenizer
+from translator import *
+from torchtext.data.utils import get_tokenizer
 import torch
 
 app = Flask(__name__)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-mapping = torch.load('./model/vocab')['th'].get_itos()  # number-to-word mapping
-inv_mapping = dict((v, k) for k, v in mapping.items())  # word-to-number mapping
-tokenizer = Tokenizer(engine='newmm')
+vocab_transform = torch.load('./models/vocab')['en']  # For tokenizing english text to numerical tokens
+mapping = torch.load('./models/vocab')['th'].get_itos()  # For transforming numerical output to Thai text
+tokenizer = get_tokenizer('spacy', language='en_core_web_sm')
 
-params, state = torch.load('./model/additive_Seq2SeqTransformer.pt')
-model = Seq2SeqTransformer(**params, device=device).to(device)
-model.load_state_dict(state)
-model.eval()
+# params, state = torch.load('./models/additive_Seq2SeqTransformer.pt')
+# model = Seq2SeqTransformer(**params, device=device).to(device)
+# model.load_state_dict(state)
+# model.eval()
+
+checkpoint = torch.load('./models/additive_Seq2SeqTransformer.pt')
+params = checkpoint['hyperparameters']
+
+enc = Encoder(**params, device=device)
+dec = Decoder(**params, device=device)
+
+enc.load_state_dict(checkpoint['encoder'])
+dec.load_state_dict(checkpoint['decoder'])
+
+model = Seq2SeqTransformer(**params, encoder=enc, decoder=dec, device=device).to(device)
+model.load_state_dict(checkpoint['model'])
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        prompt = request.form['prompt']
-        tokenized_prompt = ['<sos>'] + tokenizer.word_tokenize(prompt) + ['<eos>']  # tokenize then concatenate special tags to the start and end of list
-        num_tokens = [inv_mapping(k) for k in tokenized_prompt]  # convert to numerical representations
-        model_input = torch.FloatTensor(num_tokens).reshape(1, -1).to(device)  # prepare model input
-        translation = model.generate(model_input)[0]
+        prompt = request.form['prompt'].lower()
+        tokenized_prompt = ['<sos>'] + tokenizer(prompt) + ['<eos>']  # tokenize then concatenate special tags to the start and end of list
+        num_tokens = vocab_transform(tokenized_prompt)  # convert to numerical representations
+        print(num_tokens)
+        model_input = torch.tensor(num_tokens, dtype=torch.int64).reshape(1, -1).to(device)  # prepare model input
+        model_output = model.generate(model_input)[0]
+        print(model_output)
+        translation = [mapping[token.item()] for token in model_output]
 
         return render_template('home.html', output=' '.join(translation), show_text="block")
 
--- app/translator.py
+++ app/translator.py
@@ -3,18 +3,286 @@ import math
 from torch import nn
 import torch.nn.functional as F
 
+class EncoderLayer(nn.Module):
+    def __init__(self, hid_dim, n_heads, pf_dim, dropout, attn_variant, device):
+        super().__init__()
+        self.self_attn_layer_norm = nn.LayerNorm(hid_dim)
+        self.ff_layer_norm        = nn.LayerNorm(hid_dim)
+        self.self_attention       = MultiHeadAttentionLayer(hid_dim, n_heads, dropout, attn_variant, device)
+        self.feedforward          = PositionwiseFeedforwardLayer(hid_dim, pf_dim, dropout)
+        self.dropout              = nn.Dropout(dropout)
+        
+    def forward(self, src, src_mask):
+        #src = [batch size, src len, hid dim]
+        #src_mask = [batch size, 1, 1, src len]   #if the token is padding, it will be 1, otherwise 0
+        _src, _ = self.self_attention(src, src, src, src_mask)
+        src     = self.self_attn_layer_norm(src + self.dropout(_src))
+        #src: [batch_size, src len, hid dim]
+        
+        _src    = self.feedforward(src)
+        src     = self.ff_layer_norm(src + self.dropout(_src))
+        #src: [batch_size, src len, hid dim]
+        
+        return src
+    
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hid_dim, n_layers, n_heads, pf_dim, dropout, attn_variant, device, max_length = 500):
+        super().__init__()
+        self.device = device
+        self.attn_variant = attn_variant
+        self.tok_embedding = nn.Embedding(input_dim, hid_dim)
+        self.pos_embedding = nn.Embedding(max_length, hid_dim)
+        self.layers        = nn.ModuleList([EncoderLayer(hid_dim, n_heads, pf_dim, dropout, attn_variant, device)
+                                           for _ in range(n_layers)])
+        self.dropout       = nn.Dropout(dropout)
+        self.scale         = torch.sqrt(torch.FloatTensor([hid_dim])).to(self.device)
+        
+    def forward(self, src, src_mask):
+        
+        #src = [batch size, src len]
+        #src_mask = [batch size, 1, 1, src len]
+        
+        batch_size = src.shape[0]
+        src_len    = src.shape[1]
+        
+        pos        = torch.arange(0, src_len).unsqueeze(0).repeat(batch_size, 1).to(self.device)
+        #pos: [batch_size, src_len]
+        
+        src        = self.dropout((self.tok_embedding(src) * self.scale) + self.pos_embedding(pos))
+        #src: [batch_size, src_len, hid_dim]
+        
+        for layer in self.layers:
+            src = layer(src, src_mask)
+        #src: [batch_size, src_len, hid_dim]
+        
+        return src
+            
+# Default Transformer attention variant
+class ScaledAttention(nn.Module):
+    def __init__(self, head_dim):
+        super().__init__()
+        self.scale = torch.sqrt(torch.FloatTensor([head_dim])).to(device)
+
+    def forward(self, Q, K):
+        scores = torch.matmul(Q, K.permute(0, 1, 3, 2)) / self.scale
+        # scores: [batch_size, n_heads, query len, key len]
+
+        return scores
+    
+class MultiplicativeAttention(nn.Module):
+    def __init__(self, head_dim):
+        super().__init__()
+        self.W1 = nn.Linear(head_dim, head_dim)
+
+    def forward(self, Q, K):
+        scores = torch.matmul(self.W1(Q), K.permute(0, 1, 3, 2))
+        # scores: [batch_size, n_heads, query len, key len]
+
+        return scores
+    
+class AdditiveAttention(nn.Module):
+    def __init__(self, head_dim):
+        super().__init__()
+        self.W1 = nn.Linear(head_dim, head_dim)
+        self.W2 = nn.Linear(head_dim, head_dim)
+        self.V = nn.Linear(head_dim, 1)
+
+    def forward(self, Q, K):
+        Q = Q.unsqueeze(3)  # Q: [batch_size, n_heads, query len, head_dim] => [batch_size, n_heads, query len, 1, head_dim]
+        K = K.unsqueeze(2)  # Q: [batch_size, n_heads, key len, head_dim] => [batch_size, n_heads, 1, key len, head_dim]
+        features = torch.tanh(self.W1(Q) + self.W2(K))
+        # features: [batch_size, n_heads, query len, key len, head_dim]
+
+        scores = self.V(features).squeeze(-1)
+        # scores: [batch_size, n_heads, query len, key len]
+
+        return scores
+    
+class MultiHeadAttentionLayer(nn.Module):
+    def __init__(self, hid_dim, n_heads, dropout, attn_variant, device):
+        super().__init__()
+        assert hid_dim % n_heads == 0
+        self.hid_dim  = hid_dim
+        self.n_heads  = n_heads
+        self.head_dim = hid_dim // n_heads
+        self.attn_variant = attn_variant
+        
+        self.fc_q     = nn.Linear(hid_dim, hid_dim)
+        self.fc_k     = nn.Linear(hid_dim, hid_dim)
+        self.fc_v     = nn.Linear(hid_dim, hid_dim)
+        
+        self.fc_o     = nn.Linear(hid_dim, hid_dim)
+        
+        self.dropout  = nn.Dropout(dropout)
+
+        self.scale = torch.sqrt(torch.FloatTensor([self.head_dim])).to(device)
+
+        if attn_variant == 'scaled':
+            self.scaled_attention = ScaledAttention(self.head_dim)
+
+        elif attn_variant == 'multiplicative':
+            self.multiplicative_attention = MultiplicativeAttention(self.head_dim)
+        
+        elif attn_variant == 'additive':
+            self.additive_attention = AdditiveAttention(self.head_dim)
+                
+    def forward(self, query, key, value, mask = None):
+        #src, src, src, src_mask
+        #query = [batch size, query len, hid dim]
+        #key = [batch size, key len, hid dim]
+        #value = [batch size, value len, hid dim]
+        
+        batch_size = query.shape[0]
+        
+        Q = self.fc_q(query)
+        K = self.fc_k(key)
+        V = self.fc_v(value)
+        # Q: [batch_size, seq len, hid_dim]
+        
+        Q = Q.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        K = K.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        V = V.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        # Q=K=V: [batch_size, n heads, seq len, head_dim]
+        
+        # energy = self.additive_attention(Q, K)
+        if self.attn_variant == "general":
+            energy = torch.matmul(Q, K.permute(0, 1, 3, 2))
+
+        elif self.attn_variant == "scaled":
+            energy = torch.matmul(Q, K.permute(0, 1, 3, 2)) / self.scale
+            # energy = self.scaled_attention(Q, K)
+
+        elif self.attn_variant == "multiplicative":
+            energy = self.multiplicative_attention(Q, K)
+
+        elif self.attn_variant == "additive":
+            energy = self.additive_attention(Q, K)
+            
+        else:
+            raise Exception("Incorrect value for attention variant. Must be one of the following: \
+                            scaled, general, multiplicative, additive")
+        
+        #for making attention to padding to 0
+        if mask is not None:
+            energy = energy.masked_fill(mask == 0, -1e10)
+            
+        attention = torch.softmax(energy, dim = -1)
+        #attention = [batch_size, n heads, query len, key len]
+
+        x = torch.matmul(self.dropout(attention), V)
+        #[batch_size, n heads, query len, key len] @ [batch_size, n heads, value len, head_dim]
+        #x = [batch_size, n heads, query len, head dim]
+        
+        x = x.permute(0, 2, 1, 3).contiguous()  #we can perform .view
+        #x = [batch_size, query len, n heads, head dim]
+        
+        x = x.view(batch_size, -1, self.hid_dim)
+        #x = [batch_size, query len, hid dim]
+        
+        x = self.fc_o(x)
+        #x = [batch_size, query len, hid dim]
+        
+        return x, attention
+        
+
+class PositionwiseFeedforwardLayer(nn.Module):
+    def __init__(self, hid_dim, pf_dim, dropout):
+        super().__init__()
+        self.fc1 = nn.Linear(hid_dim, pf_dim)
+        self.fc2 = nn.Linear(pf_dim, hid_dim)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, x):
+        #x = [batch size, src len, hid dim]
+        x = self.dropout(torch.relu(self.fc1(x)))
+        x = self.fc2(x)
+        
+        return x
+    
+class DecoderLayer(nn.Module):
+    def __init__(self, hid_dim, n_heads, pf_dim, dropout, attn_variant, device):
+        super().__init__()
+        self.self_attn_layer_norm = nn.LayerNorm(hid_dim)
+        self.enc_attn_layer_norm  = nn.LayerNorm(hid_dim)
+        self.ff_layer_norm        = nn.LayerNorm(hid_dim)
+        self.self_attention       = MultiHeadAttentionLayer(hid_dim, n_heads, dropout, attn_variant, device)
+        self.encoder_attention    = MultiHeadAttentionLayer(hid_dim, n_heads, dropout, attn_variant, device)
+        self.feedforward          = PositionwiseFeedforwardLayer(hid_dim, pf_dim, dropout)
+        self.dropout              = nn.Dropout(dropout)
+        
+    def forward(self, trg, enc_src, trg_mask, src_mask):
+        
+        #trg = [batch size, trg len, hid dim]
+        #enc_src = [batch size, src len, hid dim]
+        #trg_mask = [batch size, 1, trg len, trg len]
+        #src_mask = [batch size, 1, 1, src len]
+        
+        _trg, _ = self.self_attention(trg, trg, trg, trg_mask)
+        trg     = self.self_attn_layer_norm(trg + self.dropout(_trg))
+        #trg = [batch_size, trg len, hid dim]
+        
+        _trg, attention = self.encoder_attention(trg, enc_src, enc_src, src_mask)
+        trg             = self.enc_attn_layer_norm(trg + self.dropout(_trg))
+        #trg = [batch_size, trg len, hid dim]
+        #attention = [batch_size, n heads, trg len, src len]
+        
+        _trg = self.feedforward(trg)
+        trg  = self.ff_layer_norm(trg + self.dropout(_trg))
+        #trg = [batch_size, trg len, hid dim]
+        
+        return trg, attention
+    
+class Decoder(nn.Module):
+    def __init__(self, output_dim, hid_dim, n_layers, n_heads, 
+                 pf_dim, dropout, attn_variant, device, max_length = 500):
+        super().__init__()
+        self.device = device
+        self.tok_embedding = nn.Embedding(output_dim, hid_dim)
+        self.pos_embedding = nn.Embedding(max_length, hid_dim)
+        self.layers        = nn.ModuleList([DecoderLayer(hid_dim, n_heads, pf_dim, dropout, attn_variant, device)
+                                            for _ in range(n_layers)])
+        self.fc_out        = nn.Linear(hid_dim, output_dim)
+        self.dropout       = nn.Dropout(dropout)
+        self.scale         = torch.sqrt(torch.FloatTensor([hid_dim])).to(device)
+        
+    def forward(self, trg, enc_src, trg_mask, src_mask):
+        
+        #trg = [batch size, trg len]
+        #enc_src = [batch size, src len, hid dim]
+        #trg_mask = [batch size, 1, trg len, trg len]
+        #src_mask = [batch size, 1, 1, src len]
+        
+        batch_size = trg.shape[0]
+        trg_len    = trg.shape[1]
+        
+        pos = torch.arange(0, trg_len).unsqueeze(0).repeat(batch_size, 1).to(self.device)
+        #pos: [batch_size, trg len]
+        
+        trg = self.dropout((self.tok_embedding(trg) * self.scale) + self.pos_embedding(pos))
+        #trg: [batch_size, trg len, hid dim]
+        
+        for layer in self.layers:
+            trg, attention = layer(trg, enc_src, trg_mask, src_mask)
+            
+        #trg: [batch_size, trg len, hid dim]
+        #attention: [batch_size, n heads, trg len, src len]
+        
+        output = self.fc_out(trg)
+        #output = [batch_size, trg len, output_dim]
+        
+        return output, attention
+
 class Seq2SeqTransformer(nn.Module):
-    def __init__(self, encoder, decoder, src_pad_idx, trg_pad_idx, trg_eos_idx, device):
+    def __init__(self, encoder, decoder, src_pad_idx, trg_pad_idx, device):
         super().__init__()
         # store the input parameters so we can retrive them later for model inferencing
         self.params = {'encoder': encoder, 'decoder': decoder,
                        'src_pad_idx': src_pad_idx,
-                       'trg_pad_idx': trg_pad_idx, 'trg_eos_idx': trg_eos_idx}
+                       'trg_pad_idx': trg_pad_idx}
         self.encoder = encoder
         self.decoder = decoder
         self.src_pad_idx = src_pad_idx
         self.trg_pad_idx = trg_pad_idx
-        self.trg_eos_idx = trg_eos_idx
         self.device = device
         
     def make_src_mask(self, src):
@@ -80,7 +348,7 @@ class Seq2SeqTransformer(nn.Module):
                 pred_token = output.argmax(2)[:, -1].unsqueeze(1)
                 trg = torch.cat((trg, pred_token), dim=1)
 
-                if pred_token.item() == self.trg_eos_idx:
+                if pred_token.item() == self.trg_pad_idx:
                     break
 
             return trg[:, 1:]
